d890cd789b174b26c6575f074a637eab20c36593
mahsa <solmahsa6@gmail.com>
2024-12-27 20:33:39 +0330

    newproject of react

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styled from "./footer.module.css"
+
+function Footer() {
+    return (
+      <div>
+        <h2 className={styled.header}> Footer </h2>
+      </div>
+    )
+  }
+export default Footer; 
+
+
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import styled from "./navbar.module.css"
+
+function Navbar() {
+    return (
+        <div className={styled.header}>
+            <h1 className="header-title"> Navbar </h1>
+        </div>
+    )
+}
+
+export default Navbar;
