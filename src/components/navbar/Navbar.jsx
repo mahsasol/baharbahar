d4929ca827253ce7f7ba800b388a18c6a61218a8
mahsa <solmahsa6@gmail.com>
2024-12-28 14:17:58 +0330

    learn react 14---add header name for navbar with const or let

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import styled from "./navbar.module.css"
 
 function Navbar() {
+    const title = "بهار سایت"
     return (
         <div className={styled.header}>
+            <h3>{title}</h3>
            <ul>
             <li>لیست مقالات</li>
             <li>مقاله جدید</li>
