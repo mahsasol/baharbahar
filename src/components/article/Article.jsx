3ef2aa1b0555c9e47d22811805d58f3bdca28087
mahsa <solmahsa6@gmail.com>
2024-12-29 21:13:18 +0330

    19-20 Add picture articles with import and create db.json

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,11 +1,14 @@
 import styled from "./article.module.css"
+import holiday from "../../assets/images/holidaysbluepik.jpg"
 
 function Article() {
     return (
         <div className={styled.articleWrapper}>
+           <img src={holiday}/>
            
-           
-           
+            <h3>متغیر ها در اسکریپت</h3>
+            <p> خواندن 5 دقیقه ای </p>
+
         </div>
     )
 }
