11118f445ab8be4dc6681158e14df1e5ec8ad113
mahsa <solmahsa6@gmail.com>
2025-01-06 01:17:48 +0330

    26- add axios and .get .then .catch

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,18 +1,24 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Article from "../../components/article/Article";
 import Navbar from "../../components/navbar/Navbar";
-import styled from "./home.module.css"
-import pic from "../../assets/images/christmas.jpg"
-
+import styled from "./home.module.css";
+import pic from "../../assets/images/christmas.jpg";
+import axios from "axios";
+import { error } from "ajv/dist/vocabularies/applicator/dependencies";
 
 function Home() {
-    const [articles, setArticles]= useState([])
+    const [articles, setArticles] = useState([]);
 
     useEffect(() => {
         //Api call
 
-     setArticles();
-
+        axios.get("http://localhost:8000/articles")
+            .then((result) => {
+                setArticles(result.data.data);
+            })
+            .catch((error)=>{
+              console.log(error);  
+            })
     }, []);
 
     return (
@@ -20,23 +26,16 @@ function Home() {
             <Navbar title="bahar blog" />
 
             <div className="container">
-
                 <h2>مقالات جدید</h2>
 
                 <div className={styled.articles}>
-
-                    {
-                        articles.map(article => (
-                            <Article key={article.id} article={article} />
-                        ))
-                    }
-
-
+                    {articles.map((article) => (
+                        <Article key={article.id} article={article} />
+                    ))}
                 </div>
-
             </div>
         </div>
-    )
+    );
 }
 
 export default Home;
