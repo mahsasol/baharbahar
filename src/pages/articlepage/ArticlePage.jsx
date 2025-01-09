4eafc76d34d6ebf9563880099bb2b7e6f9dfd610
mahsa <solmahsa6@gmail.com>
2025-01-09 22:55:25 +0330

    32-33 add useParams id,linkto-getting every single ID ,useState .

--- src/pages/articlepage/ArticlePage.jsx
+++ src/pages/articlepage/ArticlePage.jsx
@@ -1,13 +1,53 @@
 import Navbar from "../../components/navbar/Navbar";
+import styled from "../articlepage/articalpage.module.css"
+import picture from "../../assets/images/cristmastreeBall.png"
+import Footer from "../../components/footer/Footer";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { error } from "ajv/dist/vocabularies/applicator/dependencies";
 
 function ArticlePage() {
+    const [ article, setArticle]=useState({});
+
+    const params = useParams();
+    useEffect(() => {
+        axios.get(`http://localhost:8000/articles/${params.id}`)
+            .then((result) => {
+                setArticle(result.data);
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+    })
+
     return (
 
         <div>
             <Navbar title="Bahar Blog" />
 
-            <div className="container">
-                <h1>article</h1>
+            <div className={styled.articlePageWrapper}>
+
+                <div className="container">
+
+                    <h1>{article.title} </h1>
+
+                    <div className={styled.articleinfo}>
+
+                        <span> تاریخ:{article.date} </span>
+                        <span> نویسنده:{article.athor}  </span>
+                        <span> مدت زمان خواندن: {article.readingTime} دقیقه </span>
+
+                    </div>
+
+                    <img src={article.imageUrl}  />
+
+                    <p>{article.content}</p>
+
+                </div>
+
+                <Footer />
+
             </div>
         </div>
     );
